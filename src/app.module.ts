479285e9393a38adf356aeef494047e2aa4d7310
Luiz Teles <luizteles934@gmail.com>
2024-05-22 14:12:22 -0300

    feat: Importar o módulo shortener no AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './dataSourceOptions';
+import { ShortenerModule } from './shortener/shortener.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), ShortenerModule],
   controllers: [],
   providers: [],
 })
