56aaed05117235ad4f5ed0672475cb6f8f419a87
Luiz Teles <luizteles934@gmail.com>
2024-05-25 15:25:58 -0300

    feat: Ajustar o status da resposta

--- src/shortener/shortener.controller.ts
+++ src/shortener/shortener.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { HttpCode, HttpStatus } from '@nestjs/common';
 import { CreateShortenerDto } from './dto/create-shortener.dto';
 import { ShortenerService } from './shortener.service';
 
@@ -7,16 +8,19 @@ export class ShortenerController {
   constructor(private readonly shortenerService: ShortenerService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   async create(@Body() createShortenerDto: CreateShortenerDto) {
     return await this.shortenerService.create(createShortenerDto);
   }
 
   @Get(':pathname')
+  @HttpCode(HttpStatus.OK)
   async redirect(@Param('pathname') pathname: string) {
     return await this.shortenerService.redirect(pathname);
   }
 
   @Get('details/:pathname')
+  @HttpCode(HttpStatus.OK)
   async findOne(@Param('pathname') pathname: string) {
     return await this.shortenerService.findOne(pathname);
   }
