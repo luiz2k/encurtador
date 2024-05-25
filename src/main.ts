80fe401b8f033fc43e25312249d331c802c030b3
Luiz Teles <luizteles934@gmail.com>
2024-05-25 14:49:08 -0300

    feat: Configurar o Swagger na aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Encurtador de URL - API')
+    .setDescription('API para encurtar URLs')
+    .setVersion('1.0')
+    .addTag('shortener')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3333);
 }
