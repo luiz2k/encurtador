2cf8785e15f28612ea5117f1f58234a226ec51a6
Luiz Teles <luizteles934@gmail.com>
2024-05-30 16:16:46 -0300

    feat: Configurar o banco de dados com o nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from './dataSourceOptions';
 import { ShortenerModule } from './shortener/shortener.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), ShortenerModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mongodb',
+        host: configService.get<string>('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+      inject: [ConfigService],
+    }),
+    ShortenerModule,
+  ],
   controllers: [],
   providers: [],
 })
